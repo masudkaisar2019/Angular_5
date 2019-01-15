457cdaee595f8c55fd6698f18acb2c66e8073e2b
masudtek <masud08eee@gmail.com>
2019-01-14 21:35:17 -0500

    update get and post

--- getPost/src/app/app.module.ts
+++ getPost/src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule} from '@angular/common/http';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 
@@ -13,7 +11,6 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
